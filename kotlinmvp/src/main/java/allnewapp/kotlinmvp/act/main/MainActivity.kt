ac7d4d3ab34855d5ae91fadbb34432bd16ba0367
heping <459582629@qq.com>
2018-01-31 22:47:44 +0800

    新建主界面Act

---
+++ kotlinmvp/src/main/java/allnewapp/kotlinmvp/act/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package allnewapp.kotlinmvp.act.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import allnewapp.kotlinmvp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
